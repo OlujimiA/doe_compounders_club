2f51bf43afd4c382ff1160c0ea3d8408043eb45e
OlujimiA <184277082+OlujimiA@users.noreply.github.com>
2026-07-18 15:45:09 +0100

    Make website mobile-friendly

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-function join(){
-    window.open("https://whatsapp.com/channel/0029VbBQLXQA89MaQLXx0M3D", "_blank").focus()
+function join() {
+    window.open(
+        "https://whatsapp.com/channel/0029VbBQLXQA89MaQLXx0M3D",
+        "_blank"
+    ).focus();
 }
+
+const hamburger = document.getElementById("hamburger");
+const nav = document.getElementById("nav-menu");
+
+hamburger.addEventListener("click", () => {
+    nav.classList.toggle("active");
+});
+
+const navLinks = nav.querySelectorAll("a");
+
+navLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        nav.classList.remove("active");
+    });
+});
